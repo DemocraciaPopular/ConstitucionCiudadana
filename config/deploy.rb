2d4371e98174d35abe6211305510081016675523
ConsDev <organicapopular@proton.me>
2022-10-03 03:56:04 -0400

    lolo

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :application, "consul"
 set :deploy_to, deploysecret(:deploy_to)
 set :ssh_options, port: deploysecret(:ssh_port)
 
-set :repo_url, "git@github.com:DemocraciaPopular/ConstitucionCiudadana.git"
+set :repo_url, "https://github.com/DemocraciaPopular/ConstitucionCiudadana.git"
 
 set :revision, `git rev-parse --short #{fetch(:branch)}`.strip
 
